40c34a4527ff5d9d6a847049046c76e9c2864d26
RoyDefined <145583009+RoyDefined@users.noreply.github.com>
2025-03-23 13:51:33 +0100

    Implemented the base for improved configuration of engine types

---
+++ src/PubDoomer.UI/Engine/EngineRunConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PubDoomer.Project.Engine;
+
+namespace PubDoomer.Engine;
+
+/// <summary>
+/// Represents the base of an engine's configuration to run with.
+/// </summary>
+public abstract partial class EngineRunConfiguration : ObservableObject
+{
+    [ObservableProperty] private EngineContext _context;
+}
---
+++ src/PubDoomer.UI/Engine/EngineTypes/ZandronumEngineRunConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PubDoomer.Project.Engine;
+
+namespace PubDoomer.Engine;
+
+/// <summary>
+/// Represents the base configuration for Zandronum engines to run with.
+/// </summary>
+public sealed partial class ZandronumEngineRunConfiguration : ZdoomDerivedEngineRunConfiguration
+{
+    /// <summary>
+    /// The file path of configuration to apply to the running game.
+    /// </summary>
+    [ObservableProperty] private string? _configurationFilePath;
+    
+    /// <summary>
+    /// The game mode to run the game in.
+    /// </summary>
+    [ObservableProperty] private string? _gameMode;
+}
