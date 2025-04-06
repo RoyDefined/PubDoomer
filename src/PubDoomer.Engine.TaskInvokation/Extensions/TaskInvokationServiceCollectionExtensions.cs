0fab814ea525014ce22cc764913f01787108fe01
RoyDefined <145583009+RoyDefined@users.noreply.github.com>
2025-04-06 13:20:53 +0200

    Move task validation extension code to a separate file in the project

---
+++ src/PubDoomer.Engine.TaskInvokation/Extensions/TaskInvokationServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PubDoomer.Engine.TaskInvokation.Orchestration;
+using PubDoomer.Engine.TaskInvokation.TaskDefinition;
+using System.Threading.Tasks;
+
+namespace PubDoomer.Engine.TaskInvokation.Extensions;
+
+public static class TaskInvokationServiceCollectionExtensions
+{
+    public static IServiceCollection AddTaskInvokation(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection
+            .AddTransient(GetTaskHandlerOrDefault)
+            .AddTransient(GetTaskValidatorOrDefault)
+            .AddTransient<ProjectTaskOrchestrator>();
+    }
+
+    private static ProjectTaskOrchestratorHandlerProviderDelegate GetTaskHandlerOrDefault(IServiceProvider provider)
+    {
+        return (task, context)
+            => ActivatorUtilities.CreateInstance(provider, task.Task.HandlerType, task, context) as ITaskHandler;
+    }
+
+    private static ProjectTaskOrchestratorValidatorProviderDelegate GetTaskValidatorOrDefault(IServiceProvider provider)
+    {
+        return (task)
+            => ActivatorUtilities.CreateInstance(provider, task.ValidatorType!, task) as ITaskValidator;
+    }
+}
