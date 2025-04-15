608574f735b74ff2946ed51d4d5747f0bee48ae1
RoyDefined <145583009+RoyDefined@users.noreply.github.com>
2025-04-15 15:55:35 +0200

    Improved the combo boxes on the edit map dialogue and added an explicit "None" option

--- src/PubDoomer.UI/ViewModels/Dialogues/ConfigureEditMapViewModel.cs
+++ src/PubDoomer.UI/ViewModels/Dialogues/ConfigureEditMapViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using Avalonia.Collections;
 using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 using PubDoomer.Project;
@@ -23,19 +25,19 @@ public partial class ConfigureEditMapViewModel : PageViewModel
     /// <summary>
     /// The collection of IWad instances that can be selected.
     /// </summary>
-    [ObservableProperty] private ObservableCollection<IWadContext> _selectableIWads;
+    [ObservableProperty] private ObservableCollection<KeyValuePair<string, IWadContext?>> _selectableIWads;
     
     /// <summary>
     /// The collection of configurations that can be selected.
     /// <br /> Currently this only lists names and not anything else. UDB is aware of the configurations.
     /// </summary>
-    [ObservableProperty] private ObservableCollection<string> _selectableConfigurations;
+    [ObservableProperty] private ObservableCollection<KeyValuePair<string, string?>> _selectableConfigurations;
     
     /// <summary>
     /// The collection of compilers that can be selected.
     /// <br /> This list is determined the same way Ultimate Doombuilder determines compilers.
     /// </summary>
-    [ObservableProperty] private ObservableCollection<UdbCompiler> _selectableCompilers;
+    [ObservableProperty] private ObservableCollection<KeyValuePair<string, UdbCompiler?>> _selectableCompilers;
     
     /// <summary>
     /// The IWad configured to be opened.
@@ -61,19 +63,30 @@ public partial class ConfigureEditMapViewModel : PageViewModel
 
         // Design-time data.
         _udbExecutableFilePath = new LocalSettings().Configurations[SavingStatics.UdbExecutableFilePathKey]!;
-        SelectableIWads = new CurrentProjectProvider().ProjectContext!.IWads;
+        
+        var selectableIWadsIterator = new CurrentProjectProvider().ProjectContext!.IWads.Select(x =>
+            new KeyValuePair<string, IWadContext>(x.Name!, x));
+        SelectableIWads =
+        [
+            AddNone<IWadContext>(),
+            ..selectableIWadsIterator!
+        ];
+            
         SelectableConfigurations =
         [
-            "Boom_Doom2Doom",
-            "Boom_DoomDoom",
-            "Doom_Doom2Doom",
-            "Doom_DoomDoom",
+            AddNone<string>(),
+            new("Boom_Doom2Doom", "Boom_Doom2Doom"),
+            new("Boom_DoomDoom", "Boom_DoomDoom"),
+            new("Doom_Doom2Doom", "Doom_Doom2Doom"),
+            new("Doom_DoomDoom", "Doom_DoomDoom"),
         ];
+        
         SelectableCompilers =
         [
-            new("ACC", "ACC.cfg"),
-            new("BCC", "BCC.cfg"),
-            new("BCC (fork)", "ZT-BCC.cfg"),
+            AddNone<UdbCompiler>(),
+            new("ACC", new("ACC", "ACC.cfg")),
+            new("BCC", new("BCC", "BCC.cfg")),
+            new("BCC (fork)", new("BCC (fork)", "ZT-BCC.cfg")),
         ];
     }
     
@@ -87,11 +100,33 @@ public partial class ConfigureEditMapViewModel : PageViewModel
         UdbCompiler? selectedCompiler)
     {
         UdbExecutableFilePath = udbExecutableFilePath;
-        SelectableIWads = new ObservableCollection<IWadContext>(iWadContexts);
-        SelectableConfigurations = new ObservableCollection<string>(configurations);
-        SelectableCompilers = new ObservableCollection<UdbCompiler>(compilers);
+        
+        var selectableIWadsIterator = iWadContexts.Select(x => new KeyValuePair<string, IWadContext>(x.Name!, x));
+        SelectableIWads =
+        [
+            AddNone<IWadContext>(),
+            ..selectableIWadsIterator!
+        ];
+        
+        var selectableConfigurationsIterator = configurations.Select(x => new KeyValuePair<string, string>(x, x));
+        SelectableConfigurations = 
+        [
+            AddNone<string>(),
+            ..selectableConfigurationsIterator!
+        ];
+        
+        var selectableCompilersIterator = compilers.Select(x => new KeyValuePair<string, UdbCompiler>(x.Name, x));
+        SelectableCompilers =
+        [
+            AddNone<UdbCompiler>(),
+            ..selectableCompilersIterator!
+        ];
+        
         SelectedIWad = selectedIWad;
         SelectedConfiguration = selectedConfiguration;
         SelectedCompiler = selectedCompiler;
     }
+    
+    private static KeyValuePair<string, TValue?> AddNone<TValue>()
+        where TValue: class => new("None", null);
 }
